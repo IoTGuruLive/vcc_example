de59c2db82740503fd43fb198291b72f32661932
The IoT Guru <info@iotguru.live>
2018-11-21 10:15:31 +0100

    Description update

--- vcc_example.ino
+++ vcc_example.ino
@@ -1,3 +1,22 @@
+/**
+ * VCC send example
+ *
+ *
+ * You need to sign up for create your own node and field: https://iotguru.live/signup
+ * 
+ * The variables of the API call:
+ *
+ * {nodeKey} - the API key of your node
+ * {field} - the field of the node
+ * {value} - the measurement as number
+ *
+ * http://api.iotguru.live/measurement/create/create/{nodeKey}/{field}/{value}
+ *
+ * See the example HTTP call below (don't forget to replace values!).
+ *
+ * Let us 5-10 minutes to begin the aggreation of your measurements and displays them on fancy charts... :)
+ *
+ */
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
@@ -26,7 +45,6 @@ void loop()
     http.begin("http://api.iotguru.live/measurement/create/sTs5N5dlTmhbPT-ISHZApQ/battery/" + vcc);
     Serial.println("Result code: " + String(http.GET()));
     http.end();
-    
+
     delay(10000);
 }
-
